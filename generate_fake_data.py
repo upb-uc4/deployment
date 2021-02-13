3bb6789c0ea8fca247a5127223fe3d02a7734623
Paul Kramer <pkramer1503@gmail.com>
2021-02-13 11:59:57 +0100

    Reformat script

--- generate_fake_data.py
+++ generate_fake_data.py
@@ -16,8 +16,21 @@ EXAM_REG_COUNT = 10
 COURSE_COUNT = 20
 
 ROLES = ["Student", "Admin", "Lecturer"]
-FIELDS_OF_STUDY = ["Computer Science", "Chemistry", "Biology", "Physics", "Religion", "Sociology"]
-MODULE_PREFICES = ["Topics of", "Introduction to", "Applied", "Theorotical", "Experimental"]
+FIELDS_OF_STUDY = [
+    "Computer Science",
+    "Chemistry",
+    "Biology",
+    "Physics",
+    "Religion",
+    "Sociology",
+]
+MODULE_PREFICES = [
+    "Topics of",
+    "Introduction to",
+    "Applied",
+    "Theorotical",
+    "Experimental",
+]
 COURSE_TYPES = ["Lecture", "Project Group", "Seminar"]
 
 fake = Faker("en-US")
@@ -28,14 +41,18 @@ fake.random.seed(4321)
 
 basepath = os.path.join("defaults", "generated")
 lecturer_ids = []
-modules_by_field_of_study = {field: [] for field in FIELDS_OF_STUDY}  # Dict with modules mapped to their field of study (to let generated data appear less random)
+modules_by_field_of_study = {
+    field: [] for field in FIELDS_OF_STUDY
+}  # Dict with modules mapped to their field of study (to let generated data appear less random)
 
 
 def generate_user(role: str):
     assert role in ROLES
 
     profile = fake.simple_profile()
-    while len(profile["name"].split(" ")) != 2:  # Some names where like Mr. John Smith...
+    while (
+        len(profile["name"].split(" ")) != 2
+    ):  # Some names where like Mr. John Smith...
         profile = fake.simple_profile()
 
     return {
@@ -93,17 +110,27 @@ def generate_exam_reg(all_modules: list):
         # Choose existing or generate new module for this exam reg
         if random.random() < 0.8 or not my_modules:
             new_module = {
-                "id": "M." + str(fake.pyint(0, 9999)).zfill(4) + "." + str(fake.pyint(0, 99999)).zfill(5),
+                "id": "M."
+                + str(fake.pyint(0, 9999)).zfill(4)
+                + "."
+                + str(fake.pyint(0, 99999)).zfill(5),
                 "name": random.choice(MODULE_PREFICES) + " " + field_of_study,
             }
             all_modules[field_of_study].append(new_module)
             my_modules.append(new_module)
-        elif field_of_study in modules_by_field_of_study and modules_by_field_of_study[field_of_study]:
+        elif (
+            field_of_study in modules_by_field_of_study
+            and modules_by_field_of_study[field_of_study]
+        ):
             module_cand = random.choice(modules_by_field_of_study[field_of_study])
             if module_cand and module_cand not in my_modules:
                 my_modules.append(module_cand)
     return {
-        "name": random.choice(["Bachelor", "Master"]) + " " + field_of_study + " v" + str(fake.pyint(1, 8)),
+        "name": random.choice(["Bachelor", "Master"])
+        + " "
+        + field_of_study
+        + " v"
+        + str(fake.pyint(1, 8)),
         "active": True,
         "modules": my_modules,
     }
@@ -111,8 +138,14 @@ def generate_exam_reg(all_modules: list):
 
 def generate_course():
     lecturer = random.choice(lecturer_ids)
-    flatten = lambda list_to_flatten: [item for sub_list in list_to_flatten for item in sub_list]
-    all_module_ids = set(map(lambda module: module.get("id"), flatten(modules_by_field_of_study.values())))
+    flatten = lambda list_to_flatten: [
+        item for sub_list in list_to_flatten for item in sub_list
+    ]
+    all_module_ids = set(
+        map(
+            lambda module: module.get("id"), flatten(modules_by_field_of_study.values())
+        )
+    )
     module_ids = random.sample(all_module_ids, random.randint(1, 4))
 
     return {
@@ -138,23 +171,43 @@ def write_to_file(data, _dir, filename):
     with open(os.path.join(directory, filename), "w+") as f:
         f.write(data)
 
+
 def json_dump_dict(data: dict):
     return json.dumps(data, indent=4)
 
+
 for i in range(ADMIN_COUNT):
-    write_to_file(json_dump_dict(generate_student()), "admins", str(i).zfill(2) + ".json")
+    write_to_file(
+        json_dump_dict(generate_student()), "admins", str(i).zfill(2) + ".json"
+    )
 
 for i in range(STUDENT_COUNT):
-    write_to_file(json_dump_dict(generate_student()), "students", str(i).zfill(2) + ".json")
+    write_to_file(
+        json_dump_dict(generate_student()), "students", str(i).zfill(2) + ".json"
+    )
 
 for i in range(LECTURER_COUNT):
-    write_to_file(json_dump_dict(generate_lecturer(lecturer_ids)), "lecturers", str(i).zfill(2) + ".json")
+    write_to_file(
+        json_dump_dict(generate_lecturer(lecturer_ids)),
+        "lecturers",
+        str(i).zfill(2) + ".json",
+    )
 
 for i in range(EXAM_REG_COUNT):
-    write_to_file(json_dump_dict(generate_exam_reg(modules_by_field_of_study)), "examRegs", str(i).zfill(2) + ".json")
+    write_to_file(
+        json_dump_dict(generate_exam_reg(modules_by_field_of_study)),
+        "examRegs",
+        str(i).zfill(2) + ".json",
+    )
 
 for i in range(COURSE_COUNT):
-    write_to_file(json_dump_dict(generate_course()), "courses", str(i).zfill(2) + ".json")
+    write_to_file(
+        json_dump_dict(generate_course()), "courses", str(i).zfill(2) + ".json"
+    )
 
 print("Done! 😎")
-print("Generated: {} Admins, {} Students, {} Lecturers, {} Exam Regs and {} Courses".format(ADMIN_COUNT, STUDENT_COUNT, LECTURER_COUNT, EXAM_REG_COUNT, COURSE_COUNT))
+print(
+    "Generated: {} Admins, {} Students, {} Lecturers, {} Exam Regs and {} Courses".format(
+        ADMIN_COUNT, STUDENT_COUNT, LECTURER_COUNT, EXAM_REG_COUNT, COURSE_COUNT
+    )
+)
